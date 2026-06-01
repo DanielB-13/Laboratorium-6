2304a60db3f5653b1d5b46d04500ff919f43ef5e
DanielB-13 <pakosz@student.agh.edu.pl>
2026-06-01 22:50:21 +0200

    Update main.js

--- src/main.js
+++ src/main.js
@@ -1,51 +1,74 @@
 import "./style.css";
 import dayjs from "dayjs";
 
-document.querySelector("form").addEventListener("submit", (e) => {
-  e.preventDefault();
-
-  const input = document.getElementById("birthday").value;
-
-  const selectedDate = dayjs(input);
-
-  const day = selectedDate.date();
-  const month = selectedDate.month() + 1;
-
-  const now = dayjs();
-  const year = now.year();
-
-  let birthday = dayjs(`${year}-${month}-${day}`, "YYYY-M-D");
-  let nextBirthday = dayjs(`${now.year()}-${month}-${day}`, "YYYY-M-D");
-
-  if (nextBirthday.isBefore(now, "day")) {
-  nextBirthday = nextBirthday.add(1, "year");
+const fetchArticles = async () => {
+  try {
+    const response = await fetch(
+    'https://bjkypruheektazlbbfua.supabase.co/rest/v1/article?select=*', {
+      headers: {
+        apikey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImJqa3lwcnVoZWVrdGF6bGJiZnVhIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Nzk2NzA0NjksImV4cCI6MjA5NTI0NjQ2OX0.mpNQ51jJ-AU1KX8dWqSgyqhYXBi-77GbOLSrvJmG-7c',
+        Authorization: 'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImJqa3lwcnVoZWVrdGF6bGJiZnVhIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Nzk2NzA0NjksImV4cCI6MjA5NTI0NjQ2OX0.mpNQ51jJ-AU1KX8dWqSgyqhYXBi-77GbOLSrvJmG-7c',
+      },
+    });
+  const data = await response.json();
+  console.log(data);
+  return data;
+  } catch (error) {
+  console.error('Fetch error:', error);
   }
-  if (birthday.isAfter(now, "day")) {
-    birthday = birthday.subtract(1, "year");
-  }
+};
 
-  const diffDays = now.diff(birthday, "day");
-  const diffWeeks = nextBirthday.diff(now, "week");
-  
-  if (diffDays == 0) {
-    alert("Wszystkiego najlepszego!");
-    return;
-  }
+async function renderArticles() {
+  const articles = await fetchArticles();
 
-  if (diffWeeks == 0) {
-    document.getElementById("dialog-weeks").textContent =
-  `Masz urodziny w tym tygodniu!`;
-  } else {
-    document.getElementById("dialog-weeks").textContent =
-  `Do urodzin zostało: ${diffWeeks} tygodni`;
-  }
-  document.getElementById("dialog-days").textContent =
-  `Od urodzin minęło: ${diffDays} dni`;
+  const container = document.getElementById('articles');
 
-  document.getElementById("my-dialog").showModal();
+  articles.forEach(article => {
+    const element = document.createElement('article');
+
+    element.innerHTML = `
+      <h1 class="text-primary text-2xl font-bold">${article.title}</h1>
+      <h2>${article.subtitle}</h2>
+      <h3>Autor: ${article.author}</h3>
+      <h4>Data: ${dayjs(article.created_at).format("DD-MM-YYYY")}</h4>
+      <p>${article.content}</p>
+    `;
+
+    container.appendChild(element);
+  });
+}
+
+const createNewArticle = async (title, subtitle, author, content) => {
+  try {
+    const response = await fetch('https://bjkypruheektazlbbfua.supabase.co/rest/v1/article', {
+    method: 'POST',
+    headers: {
+      apikey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImJqa3lwcnVoZWVrdGF6bGJiZnVhIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Nzk2NzA0NjksImV4cCI6MjA5NTI0NjQ2OX0.mpNQ51jJ-AU1KX8dWqSgyqhYXBi-77GbOLSrvJmG-7c',
+      Authorization: 'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImJqa3lwcnVoZWVrdGF6bGJiZnVhIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Nzk2NzA0NjksImV4cCI6MjA5NTI0NjQ2OX0.mpNQ51jJ-AU1KX8dWqSgyqhYXBi-77GbOLSrvJmG-7c',
+      'Content-Type' : 'application/json' ,
+  },
+  body: JSON.stringify({ title, subtitle, author, content }),
   });
 
-document.getElementById("close-x").addEventListener("click", () => {
-  document.getElementById("my-dialog").close();
+  if (response.status !== 201) {
+    throw new Error(`Status: ${response.status}`);
+  }
+  } catch (error) {
+    console.error('Fetch error:' , error);
+  }
+};
+/*
+document.getElementById('articleForm').addEventListener('submit', async (e) => {
+  e.preventDefault();
+
+  const title = document.getElementById('title').value;
+  const subtitle = document.getElementById('subtitle').value;
+  const author = document.getElementById('author').value;
+  const content = document.getElementById('content').value;
+
+  await createNewArticle(title, subtitle, author, content);
+
+  await renderArticles(); // 🔥 TU
 });
-  
+*/
+renderArticles();  
